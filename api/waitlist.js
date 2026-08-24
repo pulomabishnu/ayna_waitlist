5f369a41e43c500d30033c7f939520ac7f2cb666
Ameera Omar <ameeraomar@Ameeras-MacBook-Pro.local>
2026-08-24 16:21:56 -0400

    Prevent false waitlist timeout errors

--- api/waitlist.js
+++ api/waitlist.js
@@ -4,7 +4,7 @@ const SCRIPT_URL =
   process.env.WAITLIST_SCRIPT_URL ||
   "https://script.google.com/macros/s/AKfycbzqazFjOhF4xnNOMKfVR99AvnJNzbC3-6qHWIHTBGbddLuKU9OjnneewfLWXjJlsdfvJQ/exec";
 
-const WEBHOOK_TIMEOUT_MS = 10000;
+const WEBHOOK_TIMEOUT_MS = 20000;
 
 function isValidEmail(email) {
   return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
